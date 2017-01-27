2a61ecf1abe42388ebccdcadcec1617d5a755a4c
Dan DeVere <danieldevere285@gmail.com>
2017-01-27 00:16:58 -0500

    Added a search bar and tableview.  Search bar puts in fake results for now.  implemented no results found

---
+++ StoreSearch/SearchResult.swift
@@ -0,0 +1,14 @@
+//
+//  SearchResult.swift
+//  StoreSearch
+//
+//  Created by DANIEL DE VERE on 1/26/17.
+//  Copyright (c) 2017 DANIEL DE VERE. All rights reserved.
+//
+
+import Foundation
+
+class SearchResult {
+    var name = ""
+    var artistName = ""
+}
