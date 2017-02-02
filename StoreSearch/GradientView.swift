b73b3f3ef958a979c1b38e79bd6cf6f8d5e4bcc3
Dan DeVere <danieldevere285@gmail.com>
2017-02-02 10:26:26 -0500

    changed font sizes to dynamic for accessibility

---
+++ StoreSearch/GradientView.swift
@@ -0,0 +1,21 @@
+//
+//  GradientView.swift
+//  StoreSearch
+//
+//  Created by DANIEL DE VERE on 2/2/17.
+//  Copyright (c) 2017 DANIEL DE VERE. All rights reserved.
+//
+
+import UIKit
+
+class GradientView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
