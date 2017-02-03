990a58800db4061db6c5758480fa24f0e4e21504
Dan DeVere <danieldevere285@gmail.com>
2017-02-03 12:12:01 -0500

    added a state enum to search class

--- StoreSearch/Search.swift
+++ StoreSearch/Search.swift
@@ -11,9 +11,14 @@ import Foundation
 typealias SearchComplete = (Bool) -> Void
 
 class Search {
-    var searchResults = [SearchResult]()
-    var hasSearched = false
-    var isLoading = false
+    enum State {
+        case NotSearchedYet
+        case Loading
+        case NoResults
+        case Results([SearchResult])
+    }
+    
+    private(set) var state: State = .NotSearchedYet
     
     enum Category: Int {
         case All = 0
@@ -38,34 +43,30 @@ class Search {
         println("Searching...")
         if !text.isEmpty {
             dataTask?.cancel()
-            isLoading = true
-            hasSearched = true
-            searchResults = [SearchResult]()
+            state = .Loading
             let url = urlWithSearchText(text, category: category)
             let session = NSURLSession.sharedSession()
             dataTask = session.dataTaskWithURL(url, completionHandler: {
                 data, response, error in
+                self.state = .NotSearchedYet
                 var success = false
                 if let error = error {
                     if error.code == -999 { return }
                 } else if let httpResponse = response as? NSHTTPURLResponse {
                     if httpResponse.statusCode == 200 {
                         if let dictionary = self.parseJSON(data) {
-                            self.searchResults = self.parseDictionary(dictionary)
-                            self.searchResults.sort(<)
-                            
-                            println("Success!")
-                            self.isLoading = false
+                            var searchResults = self.parseDictionary(dictionary)
+                            if searchResults.isEmpty {
+                                self.state = .NoResults
+                            } else {
+                                searchResults.sort(<)
+                                self.state = .Results(searchResults)
+                            }
                             success = true
                         }
                     }
                 }
-                if !success {
-                    println("Failure! \(response)")
-                    self.hasSearched = false
-                    self.isLoading = false
-
-                }
+            
                 dispatch_async(dispatch_get_main_queue(), {
                     completion(success)
                 })
